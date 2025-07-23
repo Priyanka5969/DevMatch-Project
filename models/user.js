c818b03dc4f664c5333a08ff1e6823d972b15034
Priyanka.Vishe <vishepriyanka0@gmail.com>
2025-07-23 12:25:25 +0530

    Created user model and userSchema

---
+++ models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength:50,
+    },
+    lastName:{
+        type: String
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/ // regex for email validation
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 128,
+        validate(value) {
+            if(!validator.isStrongPassword(value)){
+                throw new Error('Enter Strong Password');
+            }
+        },  //validating using validator package
+    },
+    age:{
+        type: Number,
+        min: 18,
+    },
+    gender: {
+        type: String,
+        // enum: ["male", "female", "other"],
+        enum: {
+            values: ["male", "female", "other"],
+            message: `{VALUE} is not a valid gender`
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value) {
+          if (!validator.isURL(value)) {
+            throw new Error("Invalid Photo URL");
+          }
+        },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+        type: [String],
+    }
+}, {
+    timestamps: true,
+}
+
+);
+
+const User = mongoose.model('User', userSchema);
